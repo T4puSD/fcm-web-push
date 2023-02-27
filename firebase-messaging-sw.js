2107b8117cc6d1743014f738073c2421b088d42d
t4pusd <sdtapusd20@gmail.com>
2023-02-27 14:20:12 +0600

    init project

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+//service worker file to push notification in the background
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+);
+
+// add your own firebase app credentials here
+firebase.initializeApp({
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+});
+
+const messaging = firebase.messaging();
